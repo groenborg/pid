da354b51090fbd21c09caa629639b9ae429ef605
robertelving <vivalaklask@gmail.com>
2015-05-07 11:16:28 +0200

    Finished functionality For PID_Control

---
+++ pid_stabilizer.cpp
@@ -0,0 +1,48 @@
+#include <Arduino.h>
+#include <sliding_buffer.h> 
+#include "pid_stabilizer.h"
+
+PidControl::PidControl(float Kp, float Ki, float Kd, int bufferSize){
+	_Kp = Kp;
+	_Ki = Ki;
+	_Kd = Kd;
+	_buffer = new SlidingBuffer(bufferSize);
+
+	_lastError = 0;
+}
+
+PidControl::~PidControl(){}
+
+float PidControl::proportional(float error){
+	return _Kp * error;
+}
+
+float PidControl::integral(float error){
+	return _Ki * _buffer->add(error);
+}
+
+float PidControl::derivative(float error, float lastError){
+	return _Kd * (error - lastError);
+}
+
+void PidControl::setProportionalConstant(float proportionalValue){
+	_Kp = proportionalValue;
+}
+void PidControl::setIntegralConstant(float integralValue){
+	_Ki = integralValue;
+}
+void PidControl::setDerivativeConstant(float derivativeValue){
+	_Kd = derivativeValue;
+}
+void PidControl::setBufferSize(int bufferSize){
+	_buffer = new SlidingBuffer(bufferSize);
+}
+
+float PidControl::calcPID(float error){
+
+	float val = proportional(error) + integral(error) + derivative(error , _lastError); 
+	_lastError = error;
+
+	return val;
+
+}
--- pid_stabilizer.h
+++ pid_stabilizer.h
@@ -2,30 +2,38 @@
 #define pid_stabilizer_h
 
 #include <Arduino.h>
+#include <sliding_buffer.h>
 
-class pid_control{
+class PidControl{
 
 public:
 
 	//constructor
-	pid_control();
-	~pid_control();
+	PidControl(float Kp, float Ki, float Kd, int bufferSize);
+	~PidControl();
 
 	//calculate each individual values
-	float proportional();
-	float integral();
-	float derivative();
+	float proportional(float error);
+	float integral(float error);
+	float derivative(float error, float lastError);
 
 	//set each constant value
 	void setProportionalConstant(float proportionalValue);
 	void setIntegralConstant(float integralValue);
-	void setDerivativeConstant();
+	void setDerivativeConstant(float derivativeValue);
+	void setBufferSize(int bufferSize);
 
 	//calculates Full PID value
-	float calcPID(float derivativeValue);
+	float calcPID(float error);
 
 private:
 
+	float _Kp;
+	float _Ki;
+	float _Kd;
+
+	float _lastError;
+	SlidingBuffer *_buffer;
 
 };
 
