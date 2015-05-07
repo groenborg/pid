0d48518aeffd91adc7ab262fdf749ef2cfbe2d7e
robertelving <vivalaklask@gmail.com>
2015-05-07 10:03:00 +0200

    Wrote most of pidStabilizer.h

--- pid_stabilizer.h
+++ pid_stabilizer.h
@@ -1,8 +1,30 @@
-#ifndef pid_h
-#define pid_h
+#ifndef pid_stabilizer_h
+#define pid_stabilizer_h
 
-class pid{
+#include <Arduino.h>
 
+class pid_control{
+
+public:
+
+	//constructor
+	pid_control();
+	~pid_control();
+
+	//calculate each individual values
+	float proportional();
+	float integral();
+	float derivative();
+
+	//set each constant value
+	void setProportionalConstant(float proportionalValue);
+	void setIntegralConstant(float integralValue);
+	void setDerivativeConstant();
+
+	//calculates Full PID value
+	float calcPID(float derivativeValue);
+
+private:
 
 
 };
